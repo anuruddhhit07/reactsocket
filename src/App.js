74d06dd945f89c4792f896a9316dcce5f0886fe7
Anuruddh Kumar <anuruddh07@gmail.com>
2023-04-05 14:12:09 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import "./styles.css";
 
-export default function App() {
+
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Home from "./components/Home"
+import ChatPage from "./components/ChatPage";
+import socketIO from "socket.io-client"
+
+const socket = socketIO.connect("http://localhost:4000")
+function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <BrowserRouter>
+        <div>
+          <Routes>
+            <Route path="/" element={<Home socket={socket}/>}></Route>
+            <Route path="/chat" element={<ChatPage socket={socket}/>}></Route>
+          </Routes>
     </div>
+    </BrowserRouter>
+    
   );
 }
+
+export default App;
